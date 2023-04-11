b1d5cb779369a233814991e10ed4daa58e1d0a21
86183 <448397800@qq.com>
2023-04-11 21:59:17 +0800

    second commit

---
+++ app/src/main/java/cn/edu/jnu/x2020101345/BowlingGame.java
@@ -0,0 +1,16 @@
+package cn.edu.jnu.x2020101345;
+
+public class BowlingGame {
+
+    int score=0;
+    public void roll(int pin) {
+        score+=pin;
+
+    }
+
+    public int score() {
+        return score;
+    }
+
+
+}
---
+++ app/src/test/java/cn/edu/jnu/x2020101345/BowlingGameUnitTest.java
@@ -0,0 +1,45 @@
+package cn.edu.jnu.x2020101345;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class BowlingGameUnitTest {
+
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
+    @Test
+    public void test_20zeros()
+    {
+        repeatedRoll(0, 20);
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void test_20Ones()
+    {
+
+        repeatedRoll(1,20);
+        assertEquals(20,game.score());
+    }
+
+    private void repeatedRoll(int pin, int times) {
+        for (int i=0;i<times;i++)
+        {
+            game.roll(pin);
+        }
+    }
+
+
+}
