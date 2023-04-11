530f3d44e232815dcc8ae50d0bc8618ea20437a4
86183 <448397800@qq.com>
2023-04-11 22:06:59 +0800

    third commit

--- app/src/test/java/cn/edu/jnu/x2020101345/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020101345/BowlingGameUnitTest.java
@@ -34,6 +34,18 @@ public class BowlingGameUnitTest {
         assertEquals(20,game.score());
     }
 
+   /* @Test
+    public void test_ASpare()
+    {
+
+        game.roll(3);
+        game.roll(7);
+        game.roll(6);
+        repeatedRoll(0,17);
+        assertEquals(22,game.score());
+    }
+*/
+
     private void repeatedRoll(int pin, int times) {
         for (int i=0;i<times;i++)
         {
